b267de607579bdae2ab4cd881a01858a0a4a129c
Pol Piella Abadia <hi@polpiella.dev>
2023-01-04 15:12:06 +0000

    Adds projects

---
+++ XcodeCloudScriptsDemo/XcodeCloudScriptsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudScriptsDemoApp.swift
+//  XcodeCloudScriptsDemo
+//
+//  Created by Pol Piella Abadia on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudScriptsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
